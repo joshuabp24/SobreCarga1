6e3069c5b58ba19e5e29da75e4b45ccbd02a1166
joshuabp24 <54971712+joshuabp24@users.noreply.github.com>
2019-09-24 23:21:23 -0700

    Add files via upload

---
+++ Sobrecarga1/Operaciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecarga1
+{
+    public class Operaciones
+    {
+       //Creación de la sobrecarga de metodo
+        public double Multiplicar(double a, double b)
+        {
+
+            return a * b;
+        }
+        public double Multiplicar(double a)
+        {
+
+            return a * 2;
+        }
+        public double Multiplicar()
+        {
+
+            return 2 * 1;
+        }
+    }
+}
